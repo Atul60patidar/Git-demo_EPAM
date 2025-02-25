ae84b85191104a51543788a2430e808be1cd9007
Atul60patidar <130908873+Atul60patidar@users.noreply.github.com>
2025-02-25 14:23:16 +0530

    Add files via upload

---
+++ First_Assingment.java
@@ -0,0 +1,9 @@
+package EPAM;
+
+import java.sql.SQLOutput;
+
+public class First_Assingment {
+    public static void main(String[] args) {
+        System.out.println("my name is Atul Patidar");
+    }
+}
